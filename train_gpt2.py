da519776b93b1f4c36402ea673245c9d99cdb757
shubham <shubhamshakya61@gmail.com>
2024-09-27 18:00:03 +0530

    added dataloader and run training loop over different batches

--- train_gpt2.py
+++ train_gpt2.py
@@ -215,16 +215,37 @@ print(f"using device: {device}")
 # -----------------------------------------------------------------------------
 # simple data loader 
 import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-with open('input.txt', 'r') as f: 
-    text = f.read()
-print('Only loading first 1k charchters as model input')
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[: B*T + 1], device=device)
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        # at init load tokens from disk and store them in memory
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        # state
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B, T) # inputs
+        y = (buf[1:]).view(B, T) # targets
+        # advance the position in the tensor
+        self.current_position += B * T
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+        return x, y
+    
+train_loader = DataLoaderLite(B=4, T=32)
 
 # logits 
 model = GPT(GPTConfig())
@@ -232,6 +253,8 @@ model.to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
