73a917341c08c7fa843e59c8abdb05dbf8ebe3d2
shubham <shubhamshakya61@gmail.com>
2024-09-27 23:52:58 +0530

    train with mixed precision

--- train_gpt2.py
+++ train_gpt2.py
@@ -297,7 +297,8 @@ for i in range(50):
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
-    logits, loss = model(x, y)
+    with torch.autocast(device_type=device, dtype=torch.bfloat16):
+        logits, loss = model(x, y)  
     loss.backward()
     optimizer.step()
     # wait for GPU to finish all the work 
